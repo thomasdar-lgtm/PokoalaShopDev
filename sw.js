bab2d8f9b4ad7edaa539d1bfd5000cafd23c98f7
thomasdar-lgtm <thomas.darregert@gmail.com>
2026-09-08 17:48:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2.47d';
+const CACHE_VERSION = '2.48d';
 const CACHE_NAME = 'pokoalashopdev-v' + CACHE_VERSION;
 /* cache non versionne : la base de cartes est versionnee par son URL (?v=N),
    inutile de re-telecharger 2,7 Mo a chaque montee de version */
